53d06bf4fc424967b3ca677c8befaf1829dfe3f0
hesam <hesam.backend@gmail.com>
2026-05-18 23:31:07 -0700

    add generate_unique_slug to utils file

--- backend/utils/utils.py
+++ backend/utils/utils.py
@@ -1,3 +1,4 @@
+from django.utils.text import slugify
 
 
 class Utils(object):
@@ -7,5 +8,12 @@ class Utils(object):
         pass
 
     @classmethod
-    def save_model_object_slug(cls):
-        pass
+    def generate_unique_slug(cls, obj, model):
+        base_slug = slugify(obj.title, allow_unicode=True)
+        slug = base_slug
+        counter = 1
+
+        while model.objects.filter(slug=slug).exists():
+            slug = f'{base_slug}-{counter}'
+            counter += 1
+        return slug
