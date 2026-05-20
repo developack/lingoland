247844f0a128eaeeab287d08c4f44402a0521fd4
hesam <hesam.backend@gmail.com>
2026-05-19 23:54:15 -0700

    refactor accounts app test_serializers

--- backend/accounts/tests/test_serializers.py
+++ backend/accounts/tests/test_serializers.py
@@ -8,54 +8,61 @@ from accounts.serializers import UserDetailSerializer
 
 class TestUserRegistrationSerializer(APITestCase):
 
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(email='test@gmail.com', username='test', password='1234')
+        cls.valid_data = {'email': 'kevin@gmail.com', 'username': 'kevin', 'password': '1234', 'confirm_password': '1234'}
+        cls.wrong_email_data = {**cls.valid_data, 'email': 'test@gmail.com'}
+        cls.wrong_username_data = {**cls.valid_data, 'username': 'test'}
+        cls.wrong_password_data = {**cls.valid_data, 'confirm_password': '123'}
+
+    def setUp(self):
+        self.users_count = User.objects.count()
+
     def test_user_can_registration_successfully(self):
-        data = {'email': 'kevin@gmail.com', 'username': 'kevin', 'password': '1234', 'confirm_password': '1234'}
-        serializer = UserRegisterSerializer(data=data)
+        serializer = UserRegisterSerializer(data=self.valid_data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        self.assertEqual(User.objects.count(), 1)
-        self.assertEqual(User.objects.first().email, data['email'])
+        self.assertEqual(User.objects.count(), self.users_count + 1)
+        self.assertTrue(User.objects.filter(email=self.valid_data['email']).exists())
 
     def test_user_registration_fails_when_passwords_doesnt_match(self):
-        data = {'email': 'kevin@gmail.com', 'username': 'kevin', 'password': '1234', 'confirm_password': '123'}
-        serializer = UserRegisterSerializer(data=data)
+        serializer = UserRegisterSerializer(data=self.wrong_password_data)
         with self.assertRaises(ValidationError) as context:
             serializer.is_valid(raise_exception=True)
         self.assertIn('Passwords do not match', str(context.exception))
-        self.assertEqual(User.objects.count(), 0)
+        self.assertEqual(User.objects.count(), self.users_count)
 
     def test_user_registration_fails_when_email_already_exist(self):
-        User.objects.create_user(email='test@gmail.com', username='test', password='1234')
-        data = {'email': 'test@gmail.com', 'username': 'kevin', 'password': '1234', 'confirm_password': '123'}
-        serializer = UserRegisterSerializer(data=data)
+        serializer = UserRegisterSerializer(data=self.wrong_email_data)
         with self.assertRaises(ValidationError) as context:
             serializer.is_valid(raise_exception=True)
         self.assertIn('Email already exists', str(context.exception))
-        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.count(), self.users_count)
 
     def test_user_registration_fails_when_username_already_exist(self):
-        User.objects.create_user(email='test@gmail.com', username='test', password='1234')
-        data = {'email': 'kevin@gmail.com', 'username': 'test', 'password': '1234', 'confirm_password': '123'}
-        serializer = UserRegisterSerializer(data=data)
+        serializer = UserRegisterSerializer(data=self.wrong_username_data)
         with self.assertRaises(ValidationError) as context:
             serializer.is_valid(raise_exception=True)
         self.assertIn('Username already exists', str(context.exception))
-        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.count(), self.users_count)
 
 # ============================================================ #
 
 class TestUserLoginSerializer(APITestCase):
 
-    def setUp(self):
-        self.user = User.objects.create_user(email='test@gmail.com', username='test', password='1234')
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(email='test@gmail.com', username='test', password='1234')
+        cls.valid_data = {'email': 'test@gmail.com', 'password': '1234'}
 
     def test_user_can_login_successfully(self):
-        serializer = UserLoginSerializer(data={'email': 'test@gmail.com', 'password': '1234'})
+        serializer = UserLoginSerializer(data=self.valid_data)
         serializer.is_valid(raise_exception=True)
         self.assertEqual(serializer.validated_data['user'], self.user)
 
     def test_user_login_fails_when_credentials_does_not_match(self):
-        serializer = UserLoginSerializer(data={'email': 'test@gmail.com', 'password': '123456'})
+        serializer = UserLoginSerializer(data={**self.valid_data, 'password': '123456'})
         with self.assertRaises(ValidationError) as context:
             serializer.is_valid(raise_exception=True)
         self.assertIn('Invalid credentials', str(context.exception))
@@ -64,8 +71,10 @@ class TestUserLoginSerializer(APITestCase):
 
 class TestUserDetailSerializer(APITestCase):
 
-    def setUp(self):
-        self.user = User.objects.create_user(email='test@gmail.com', username='test', password='1234')
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(email='test@gmail.com', username='test', password='1234')
+        cls.valid_data = {'email': 'test@gmail.com', 'password': '1234'}
 
     def test_update_user_detail_successfully(self):
         data = {'user_profile': {'full_name': 'John Doe'}}
@@ -79,5 +88,5 @@ class TestUserDetailSerializer(APITestCase):
         serializer = UserDetailSerializer(instance=self.user, data=data, partial=True)
         self.assertTrue(serializer.is_valid(raise_exception=True))
         user = serializer.save()
-        self.assertEqual(user.first_name, 'John')
-        self.assertEqual(user.last_name, 'Doe')
+        self.assertEqual(user.first_name, data['first_name'])
+        self.assertEqual(user.last_name, data['last_name'])
