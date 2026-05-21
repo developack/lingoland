db8009c29f0205285273fb306134a7e11d7b8d83
hesam <hesam.backend@gmail.com>
2026-05-21 03:20:31 -0700

    refactor courses app test_models

--- backend/courses/tests/test_models.py
+++ backend/courses/tests/test_models.py
@@ -3,12 +3,14 @@ from accounts.models import User
 from courses.models import Course, Lesson, Topic, LessonActivity
 
 
-class TestCourseModels(APITestCase):
+class TestCourseModel(APITestCase):
 
     @classmethod
     def setUpTestData(cls):
+        cls.course_title = 'django course'
+        cls.course_slug = 'django-course'
         cls.user = User.objects.create_user(email='test@gamil.com', username='test', password='1234')
-        cls.course = Course.objects.create(user=cls.user, title='django')
+        cls.course = Course.objects.create(user=cls.user, title=cls.course_title)
         cls.lessons = [Lesson.objects.create(course=cls.course, title=f'l{n}') for n in range(1, 11)]
         cls.lessons = cls.course.lessons.order_by('created')
 
@@ -36,35 +38,49 @@ class TestCourseModels(APITestCase):
         self.assertEqual(result, '0% complete')
 
     def test_course_slug_generated_successfully(self):
-        course = Course.objects.create(title='django course')
-        self.assertEqual(course.slug, 'django-course')
+        course = Course.objects.create(title=self.course_title)
+        self.assertEqual(course.slug, self.course_slug)
 
     def test_course_slug_is_unique(self):
-        course = Course.objects.create(title='django course')
-        new_course = Course.objects.create(title='django course')
-        self.assertEqual(new_course.slug, 'django-course-1')
+        course = Course.objects.create(title=self.course_title)
+        new_course = Course.objects.create(title=self.course_title)
+        self.assertEqual(new_course.slug, f'{self.course_slug}-1')
         self.assertNotEqual(course.slug, new_course.slug)
 
-    # ============================================================ #
+# ============================================================ #
+
+class TestLessonModel(APITestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.lesson_title = 'django lesson'
+        cls.lesson_slug = 'django-lesson'
 
     def test_lesson_slug_generated_successfully(self):
-        lesson = Lesson.objects.create(title='django lesson')
-        self.assertEqual(lesson.slug, 'django-lesson')
+        lesson = Lesson.objects.create(title=self.lesson_title)
+        self.assertEqual(lesson.slug, self.lesson_slug)
 
     def test_lesson_slug_is_unique(self):
-        lesson = Lesson.objects.create(title='django lesson')
-        new_lesson = Lesson.objects.create(title='django lesson')
-        self.assertEqual(new_lesson.slug, 'django-lesson-1')
+        lesson = Lesson.objects.create(title=self.lesson_title)
+        new_lesson = Lesson.objects.create(title=self.lesson_title)
+        self.assertEqual(new_lesson.slug, f'{self.lesson_slug}-1')
         self.assertNotEqual(lesson.slug, new_lesson.slug)
 
-    # ============================================================ #
+# ============================================================ #
+
+class TestTopicModel(APITestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.topic_title = 'django topic'
+        cls.topic_slug = 'django-topic'
 
     def test_topic_slug_generated_successfully(self):
-        topic = Topic.objects.create(title='django topic')
-        self.assertEqual(topic.slug, 'django-topic')
+        topic = Topic.objects.create(title=self.topic_title)
+        self.assertEqual(topic.slug, self.topic_slug)
 
     def test_topic_slug_is_unique(self):
-        topic = Topic.objects.create(title='django topic')
-        new_topic = Topic.objects.create(title='django topic')
-        self.assertEqual(new_topic.slug, 'django-topic-1')
+        topic = Topic.objects.create(title=self.topic_title)
+        new_topic = Topic.objects.create(title=self.topic_title)
+        self.assertEqual(new_topic.slug, f'{self.topic_slug}-1')
         self.assertNotEqual(topic.slug, new_topic.slug)
