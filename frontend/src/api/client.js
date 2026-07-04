1fe2dfcc71638662a4989e246036455936b5b755
hesam <hesam.backend@gmail.com>
2026-07-04 15:50:08 +0330

    remove throw error in client.js

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -19,14 +19,9 @@ export const apiRequest = async (endpoint, options={}) => {
 
     try {
         const response = await fetch(endpoint, config)
-
         if (response.ok) {
             return await response.json()
         }
-
-        if (response.status === 401) return {status: 401, message: '401 error', data: response}
-        if (response.status === 403) return {status: 403, message: '403 error', data: response}
-        if (response.status === 500) return {status: 500, message: '500 error', data: response}
 
     } catch (error) {
         return error
